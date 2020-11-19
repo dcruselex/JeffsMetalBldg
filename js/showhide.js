27e7840e601363026e206321089454b734723221
dcruselex <dcruselex@gmail.com>
2020-11-19 11:58:27 -0500

    addjsscriptpageforshowhide

---
+++ js/showhide.js
@@ -0,0 +1,10 @@
+
+    function showhide() {
+var x = document.getElementById("citylist");
+if (x.style.display === "none") {
+x.style.display = "block";
+} else {
+x.style.display = "none";
+}
+}
+
